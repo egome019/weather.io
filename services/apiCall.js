938b468b3eb6da93622b7df4c688819496f98e71
erik G <egomezfax19@gmail.com>
2024-03-19 22:56:46 -0700

    created api call, need to work on routes and controller

--- services/apiCall.js
+++ services/apiCall.js
@@ -1,2 +1,10 @@
-const APICall =
-	"http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid=${API_KEY}";
+export async function fetchCity() {
+	try {
+		const cityName = document.getElementById("cityName").value;
+		const APICall = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=5&appid=${API_KEY}`;
+		const res = await fetch(APICall);
+		console.log(res.json());
+	} catch (error) {
+		console.error(error);
+	}
+}
